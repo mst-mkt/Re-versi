1a22b37c82a3b312aa22e9d521b64d9992418c7e
mst-mkt <s1f102303057@iniad.org>
2023-08-16 21:19:27 +0900

    add: 型定義を追加

--- server/commonTypesWithClient/branded.ts
+++ server/commonTypesWithClient/branded.ts
@@ -3,3 +3,5 @@ import type { z } from 'zod';
 type Branded<T extends string> = string & z.BRAND<T>;
 
 export type UserId = Branded<'UserId'>;
+
+export type RoomId = Branded<'RoomId'>;
--- server/commonTypesWithClient/models.ts
+++ server/commonTypesWithClient/models.ts
@@ -1,8 +1,26 @@
-import type { UserId } from './branded';
+import type { RoomId, UserId } from './branded';
 
 export type UserModel = {
   id: UserId;
   email: string;
   displayName: string | undefined;
   photoURL: string | undefined;
+};
+
+export type RoomModel = {
+  id: RoomId;
+  name: string;
+  board: (-1 | 0 | 1 | 2)[][];
+  turnColor: 1 | 2;
+  status: 'waiting' | 'playing' | 'finished';
+  createdAt: number;
+};
+
+export type PlayerModel = {
+  id: UserId;
+  roomId: RoomId;
+  inTime: number;
+  outTime: number | null;
+  color: 1 | 2;
+  createdAt: number;
 };
