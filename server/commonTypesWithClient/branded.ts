a1efcc9336b576023cefe2e808275f8f30e98d10
mst-mkt <s1f102303057@iniad.org>
2023-08-16 21:06:06 +0900

    remove: tasks関連を削除

--- server/commonTypesWithClient/branded.ts
+++ server/commonTypesWithClient/branded.ts
@@ -3,5 +3,3 @@ import type { z } from 'zod';
 type Branded<T extends string> = string & z.BRAND<T>;
 
 export type UserId = Branded<'UserId'>;
-
-export type TaskId = Branded<'TaskId'>;
--- server/commonTypesWithClient/models.ts
+++ server/commonTypesWithClient/models.ts
@@ -1,15 +1,8 @@
-import type { TaskId, UserId } from './branded';
+import type { UserId } from './branded';
 
 export type UserModel = {
   id: UserId;
   email: string;
   displayName: string | undefined;
   photoURL: string | undefined;
-};
-
-export type TaskModel = {
-  id: TaskId;
-  label: string;
-  done: boolean;
-  created: number;
 };
