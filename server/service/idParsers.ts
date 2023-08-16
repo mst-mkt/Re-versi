f18dccb167ca814b9e9fcddd5f84931208a60523
mst-mkt <s1f102303057@iniad.org>
2023-08-16 21:36:00 +0900

    add: idParserを追加

--- server/service/idParsers.ts
+++ server/service/idParsers.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
-import type { TaskId, UserId } from '../commonTypesWithClient/branded';
+import type { RoomId, UserId } from '../commonTypesWithClient/branded';
 
 export const userIdParser: z.ZodType<UserId> = z.string().brand<'UserId'>();
 
-export const taskIdParser: z.ZodType<TaskId> = z.string().brand<'TaskId'>();
+export const roomIdParser: z.ZodType<RoomId> = z.string().brand<'RoomId'>();
