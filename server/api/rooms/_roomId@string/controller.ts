3325057740da825bbd06d8b54078623b1f206706
mst-mkt <s1f102303057@iniad.org>
2023-08-17 21:29:04 +0900

    add: apiを実装

--- server/api/rooms/_roomId@string/controller.ts
+++ server/api/rooms/_roomId@string/controller.ts
@@ -1,38 +1,15 @@
-import type { UserModel } from '$/commonTypesWithClient/models';
-import { getUserModel } from '$/middleware/firebaseAdmin';
 import { roomsRepo } from '$/repository/roomsRepo';
-import { defineController, defineHooks } from './$relay';
-import { roomIdParser, userIdParser } from './../../../service/idParsers';
-
-export type AdditionalRequest = {
-  user: UserModel;
-};
-
-export const hooks = defineHooks(() => ({
-  preHandler: async (req, res) => {
-    const user = await getUserModel(req.cookies.session);
-
-    if (user === null) {
-      res.status(401).send();
-      return;
-    }
-
-    req.user = {
-      id: userIdParser.parse(user.uid),
-      email: user.email ?? '',
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-    };
-  },
-}));
+import { roomUseCase } from '$/usecase/roomUseCase';
+import { defineController } from './$relay';
+import { roomIdParser } from './../../../service/idParsers';
 
 export default defineController(() => ({
   get: async ({ params }) => ({
     status: 200,
     body: await roomsRepo.find(roomIdParser.parse(params.roomId)),
   }),
-  // post: async ({ params, body, user }) => ({
-  //   status: 201,
-  //   body: await roomUseCase.put(roomIdParser.parse(params.roomId), user.id, body),
-  // }),
+  post: async ({ params, body, user }) => ({
+    status: 201,
+    body: await roomUseCase.put(roomIdParser.parse(params.roomId), user.id, body.pos),
+  }),
 }));
--- server/api/rooms/_roomId@string/index.ts
+++ server/api/rooms/_roomId@string/index.ts
@@ -1,12 +1,15 @@
 import type { RoomModel } from '$/commonTypesWithClient/models';
+import type { Pos } from '$/usecase/roomUseCase';
 import type { DefineMethods } from 'aspida';
 
 export type Methods = DefineMethods<{
   get: {
     resBody: RoomModel | null;
   };
-  // post: {
-  //   reqBody: Pos;
-  //   resBody: RoomModel;
-  // };
+  post: {
+    reqBody: {
+      pos: Pos;
+    };
+    resBody: RoomModel | null;
+  };
 }>;
